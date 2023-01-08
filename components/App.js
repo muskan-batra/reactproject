28608ea9dd647e116b7671ed1e7d3ca94cb5bc36
=muskan-batra <68785899+muskan-batra@users.noreply.github.com>
2023-01-08 20:09:54 +0500

    your message for this commit

--- components/App.js
+++ components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
-// import AddContact from "./AddContact";
+ import AddContact from "./AddContact";
 // import ContactList from "./ContactList";
 
 
@@ -10,8 +10,8 @@ function App(){
   return (
   <div>
      <Header />
-     {/* <AddContact />
-     <ContactList /> */}
+     <AddContact />
+     {/* <ContactList /> */}
 
       </div>
   );
